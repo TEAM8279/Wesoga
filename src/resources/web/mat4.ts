141124797a7354e090b5f1758ffd4b012d501aee
valchap <valchap616@gmail.com>
2020-01-29 10:45:25 +0100

    Clean comments

--- src/resources/web/mat4.ts
+++ src/resources/web/mat4.ts
@@ -60,19 +60,15 @@ module mat4 {
   }
 
   export function translate(out: Float32Array, a: Float32Array, x: number, y: number, z: number) {
-    let a00, a01, a02, a03;
-    let a10, a11, a12, a13;
-    let a20, a21, a22, a23;
-
     if (a === out) {
       out[12] = a[0] * x + a[4] * y + a[8] * z + a[12];
       out[13] = a[1] * x + a[5] * y + a[9] * z + a[13];
       out[14] = a[2] * x + a[6] * y + a[10] * z + a[14];
       out[15] = a[3] * x + a[7] * y + a[11] * z + a[15];
     } else {
-      a00 = a[0]; a01 = a[1]; a02 = a[2]; a03 = a[3];
-      a10 = a[4]; a11 = a[5]; a12 = a[6]; a13 = a[7];
-      a20 = a[8]; a21 = a[9]; a22 = a[10]; a23 = a[11];
+      let a00 = a[0]; let a01 = a[1]; let a02 = a[2]; let a03 = a[3];
+      let a10 = a[4]; let a11 = a[5]; let a12 = a[6]; let a13 = a[7];
+      let a20 = a[8]; let a21 = a[9]; let a22 = a[10]; let a23 = a[11];
 
       out[0] = a00; out[1] = a01; out[2] = a02; out[3] = a03;
       out[4] = a10; out[5] = a11; out[6] = a12; out[7] = a13;
@@ -219,7 +215,7 @@ module mat4 {
    * @param {number} far Far bound of the frustum, can be null or Infinity
    */
   export function perspective(out: Float32Array, fovy: number, aspect: number, near: number, far: number) {
-    let f = 1.0 / Math.tan(fovy / 2), nf;
+    let f = 1.0 / Math.tan(fovy / 2);
     out[0] = f / aspect;
     out[1] = 0;
     out[2] = 0;
@@ -235,7 +231,7 @@ module mat4 {
     out[13] = 0;
     out[15] = 0;
     if (far != null && far !== Infinity) {
-      nf = 1 / (near - far);
+      let nf = 1 / (near - far);
       out[10] = (far + near) * nf;
       out[14] = (2 * far * near) * nf;
     } else {
--- src/resources/web/script.ts
+++ src/resources/web/script.ts
@@ -7,7 +7,7 @@
 
   // Only continue if WebGL is available and working
   if (gl === null) {
-    alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+    alert("Unable to initialize WebGL. Your browser don't support it.");
     return;
   }
 
@@ -48,11 +48,6 @@
     const texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture);
 
-    // Du fait que les images doivent être téléchargées depuis l'internet,
-    // il peut s'écouler un certain temps avant qu'elles ne soient prêtes.
-    // Jusque là, mettre un seul pixel dans la texture, de sorte que nous puissions
-    // l'utiliser immédiatement. Quand le téléchargement de la page sera terminé,
-    // nous mettrons à jour la texture avec le contenu de l'image.
     const level = 0;
     const internalFormat = gl.RGBA;
     const width = 1;
@@ -60,7 +55,7 @@
     const border = 0;
     const srcFormat = gl.RGBA;
     const srcType = gl.UNSIGNED_BYTE;
-    const pixel = new Uint8Array([255, 0, 255, 255]);  // bleu opaque
+    const pixel = new Uint8Array([255, 0, 255, 255]);
     gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
       width, height, border, srcFormat, srcType,
       pixel);
@@ -95,13 +90,10 @@
     const fragmentShader = loadShader(gl.FRAGMENT_SHADER, fsSource);
 
     // Create the shader program
-
     const shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
-
-    // If creating the shader program failed, alert
 
     if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
       alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
@@ -113,22 +105,17 @@
 
   const shaderProgram = initShaderProgram(vsSource, fsSource);
 
-  //
-  // creates a shader of the given type, uploads the source and
-  // compiles it.
-  //
+  /**
+   * Compile the given shader with the given type
+   */
   function loadShader(type: number, source: string) {
     const shader = gl.createShader(type);
 
     // Send the source to the shader object
-
     gl.shaderSource(shader, source);
 
     // Compile the shader program
-
     gl.compileShader(shader);
-
-    // See if it compiled successfully
 
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
       alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
@@ -487,13 +474,13 @@
     }
   }
 
-
+  // How to read color buffer
   {
-    const numComponents = 1;
-    const type = gl.FLOAT;
-    const normalize = false;
-    const stride = 0;
-    const offset = 0;
+    const numComponents = 1; // Values pulled per iteration
+    const type = gl.FLOAT;   // Data type
+    const normalize = false; // Don't normalize
+    const stride = 0;        // Bytes to skip between iterations
+    const offset = 0;        // Bytes to skip before first iteration
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
     gl.vertexAttribPointer(
       programInfo.attribLocations.vertexColor,
@@ -506,15 +493,13 @@
       programInfo.attribLocations.vertexColor);
   }
 
-  // Tell WebGL how to pull out the positions from the position
-  // buffer into the vertexPosition attribute.
+  // How to read position buffer
   {
-    const numComponents = 3; // pull out 3 values per iteration
-    const type = gl.FLOAT; // the data in the buffer is 32bit floats
-    const normalize = false; // don't normalize
-    const stride = 0; // how many bytes to get from one set of values to the next
-    // 0 = use type and numComponents above
-    const offset = 0; // how many bytes inside the buffer to start from
+    const numComponents = 3;
+    const type = gl.FLOAT;
+    const normalize = false;
+    const stride = 0;
+    const offset = 0;
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
     gl.vertexAttribPointer(
       programInfo.attribLocations.vertexPosition,
@@ -527,12 +512,13 @@
       programInfo.attribLocations.vertexPosition);
   }
 
+  // How to read texture buffer
   {
-    const num = 2; // chaque coordonnée est composée de 2 valeurs
-    const type = gl.FLOAT; // les données dans le tampon sont des flottants 32 bits
-    const normalize = false; // ne pas normaliser
-    const stride = 0; // combien d'octets à récupérer entre un jeu et le suivant
-    const offset = 0; // à combien d'octets du début faut-il commencer
+    const num = 2;
+    const type = gl.FLOAT;
+    const normalize = false;
+    const stride = 0;
+    const offset = 0;
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.texture);
     gl.vertexAttribPointer(programInfo.attribLocations.vertexTexture, num, type, normalize, stride, offset);
     gl.enableVertexAttribArray(programInfo.attribLocations.vertexTexture);
@@ -540,7 +526,7 @@
 
   const texture = loadTexture("grass.png");
 
-  // Tell WebGL to use our program when drawing
+  // Set the program to use
   gl.useProgram(programInfo.program);
 
   gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
@@ -583,63 +569,47 @@
       gl.viewport(0, 0, canvas.width, canvas.height);
     }
 
-    // Clear the canvas before we start drawing on it.
+    // Clear the canvas and depth buffer
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    // Indiquer à WebGL que nous voulons affecter l'unité de texture 0
+    // Use texture unit 0
     gl.activeTexture(gl.TEXTURE0);
 
-    // Lier la texture à l'unité de texture 0
+    // Link texture to texture unit 0
     gl.bindTexture(gl.TEXTURE_2D, texture);
 
-    // Indiquer au shader que nous avons lié la texture à l'unité de texture 0
+    // Use texture unit 0 with the shader
     gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
 
     {
-      // Create a perspective matrix, a special matrix that is
-      // used to simulate the distortion of perspective in a camera.
-      // Our field of view is 45 degrees, with a width/height
-      // ratio that matches the display size of the canvas
-      // and we only want to see objects between 0.1 units
-      // and 100 units away from the camera.
-
-      const fieldOfView = 45 * Math.PI / 180; // in radians
+      const fieldOfView = 45 * Math.PI / 180;
       const aspect = canvas.clientWidth / canvas.clientHeight;
       const zNear = 0.1;
       const zFar = 1000.0;
       const projectionMatrix = mat4.create();
 
-      // note: glmatrix.js always has the first argument
-      // as the destination to receive the result.
       mat4.perspective(projectionMatrix,
         fieldOfView,
         aspect,
         zNear,
         zFar);
 
-      // Set the drawing position to the "identity" point, which is
-      // the center of the scene.
-
       const modelViewMatrix = mat4.create();
 
       mat4.rotateX(modelViewMatrix, modelViewMatrix, rotX);
       mat4.rotateY(modelViewMatrix, modelViewMatrix, rotY);
 
-      mat4.translate(modelViewMatrix, // destination matrix
-        modelViewMatrix, // matrix to translate
-        -x, -y, z); // amount to translate
-
+      mat4.translate(modelViewMatrix, modelViewMatrix, -x, -y, z);
 
       const viewMatrix = mat4.create();
 
       mat4.multiply(viewMatrix, projectionMatrix, modelViewMatrix);
 
-      // Set the shader uniforms
+      // Set view matrix uniform
       gl.uniformMatrix4fv(
         programInfo.uniformLocations.uViewMatrix,
         false,
         viewMatrix);
-
     }
 
     {
@@ -651,11 +621,9 @@
     }
   }
 
-
   function draw() {
     drawScene(programInfo);
     window.requestAnimationFrame(draw);
   }
   window.requestAnimationFrame(draw);
-
 })();
